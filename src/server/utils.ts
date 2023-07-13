9599e011537f0d75e455b9c9bb1f1bcd8639c5d8
sara-ahrari <sara.ahrari97@gmail.com>
2023-07-13 14:13:12 +0200

    add import

--- src/server/utils.ts
+++ src/server/utils.ts
@@ -1,4 +1,5 @@
 import { Request } from "express"
+import fetch from "node-fetch"
 
 export function getStringQueryParam(req: Request, paramName: string): string {
   const paramValue = req.query[paramName]
